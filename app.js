56ed457848ee34bfd5518d651ce7dfb769b782c4
AhsanRiyad <riyad298@gmail.com>
2019-03-22 12:01:07 +0600

    lesson 5

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ new Vue({
     name: 'riyad' , 
     job : 'student' , 
     website : 'www.facebook.com/riyadahsan6',
-    fullTag: '<p> hello this is a complete emement </p>'
+    fullTag: '<p> hello this is a complete emement </p>',
+    x: 0 ,
+    y: 0 ,
+    age: 100
   } , 
   methods: {
   	greet : function(){
@@ -12,6 +15,16 @@ new Vue({
   	}, 
   	time : function(time1){
   		return time1 + ' ' + this.name; 
+  	},
+  	updateXY: function(event){
+  		this.x = event.offsetX ; 
+  		this.y = event.offsetY;
+  	},
+  	add : function(val){
+  		this.age += val ; 
+  	} , 
+  	subtract : function(val){
+  		this.age -= val ; 
   	}
   }
 })
