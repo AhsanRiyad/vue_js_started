7aea1c0bf25049b821424bea1a666c77777e8efb
AhsanRiyad <riyad298@gmail.com>
2019-03-22 11:43:19 +0600

    lesson 4

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
   el: '#vue-app' ,
   data: {
     name: 'riyad' , 
-    job : 'student'
+    job : 'student' , 
+    website : 'www.facebook.com/riyadahsan6',
+    fullTag: '<p> hello this is a complete emement </p>'
   } , 
   methods: {
   	greet : function(){
