d8e3d935371d75df96e4883a20ad20552750c685
AhsanRiyad <riyad298@gmail.com>
2019-03-22 12:42:04 +0600

    lesson 7

--- app.js
+++ app.js
@@ -2,32 +2,11 @@ new Vue({
   el: '#vue-app' ,
   data: {
     name: 'riyad' , 
-    job : 'student' , 
-    website : 'www.facebook.com/riyadahsan6',
-    fullTag: '<p> hello this is a complete emement </p>',
-    x: 0 ,
-    y: 0 ,
-    age: 100
+    
   } , 
   methods: {
-  	greet : function(){
-  		return 'good morning' ; 
-  	}, 
-  	time : function(time1){
-  		return time1 + ' ' + this.name; 
-  	},
-  	updateXY: function(event){
-  		this.x = event.offsetX ; 
-  		this.y = event.offsetY;
-  	},
-  	add : function(val){
-  		this.age += val ; 
-  	} , 
-  	subtract : function(val){
-  		this.age -= val ; 
-  	},
-  	showAlert : function(){
-  		alert('event fired');
+  	NameKeyUp: function(){
+  		console.log('name key up input') ; 
   	}
   }
 })
