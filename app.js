87231479528d990398f49d287883361e4acb6df5
AhsanRiyad <riyad298@gmail.com>
2019-03-22 15:30:54 +0600

    lesson 16

--- app.js
+++ app.js
@@ -1,58 +1,17 @@
-
-// example one without global object
-
-Vue.component('greeting' , {
-	template: "<p> this is component {{name}} <button v-on:click='changeName()' > change name </button> </p>" , 
-	data: function(){
-		return { name: 'riyad' }
-	} , 
-	methods : {
-		changeName: function(){
-			this.name = 'Ahsan' ;
-		}
-	}
-})
-
-
-// example 2 global object
-
-var data = {
-	name : 'riyad'
-}
-
-Vue.component('greeting2' , {
-	template: "<p> this is component {{name}} <button v-on:click='changeName()' > change name </button> </p>" , 
-	data: function(){
-		return data ; 
-	} , 
-	methods : {
-		changeName: function(){
-			this.name = 'Ahsan' ;
-		}
-	}
-})
-
-
-
-
-var one = new Vue({
-	el: '#app-one' , 
+new Vue({
+	el: '#app-one' ,
 	data: {
-		title: 'this is the title of one'
+		innerT : '' , 
+		valueI : 'hello'
 	},
-	methods : {
 
-	}
-});
-var two = new Vue({
-	el: '#app-two' , 
-	data: {
+	methods: {
+		getInput : function(){
+			this.valueI = this.$refs.input1.value;
+			this.innerT =  this.$refs.h1_tag.innerText;
+			console.log(this.$refs);
 
-	},
-	methods : {
-		changeT : function(){
-			one.title = 'title changed';
 		}
 	}
-});
 
+});
