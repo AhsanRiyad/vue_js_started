97ffbb045b0e60177ae0584b37006f34946b1043
AhsanRiyad <riyad298@gmail.com>
2019-05-13 17:28:30 +0600

    nesting component globally

---
+++ vp/src/comp1.vue
@@ -0,0 +1,12 @@
+<template>
+	<h1>hellow riyad</h1>
+</template>
+
+
+
+<script>
+	export default{
+		
+
+	}
+</script>
--- vp/src/main.js
+++ vp/src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+import Comp from './comp1.vue'
+
+Vue.component('comp1' , Comp);
+
 new Vue({
   el: '#app',
   render: h => h(App)
