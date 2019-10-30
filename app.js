b76dc05cf91916bc02fe8b8b96fb4dac6ca60402
unknown <riyad298@gmail.com>
2019-10-31 03:05:14 +0600

    http request working

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 new Vue({
-      el: '#app',
-      vuetify: new Vuetify(),
-    })
+	el: '#app',
+	vuetify: new Vuetify(),
+	data: {
+		name: 'Riyad'
+	},
+	methods: {
+
+		hi: function(){
+			alert('hi');
+		},
+		post: function(){ this.$http.post('http://jsonplaceholder.typicode.com/posts', {
+			title: 'rfaef',
+			body: 'raefaref',
+			userId: 1
+		}).then(function(data){
+			console.log(data);
+			alert(data);
+		})
+	}
+}
+
+
+
+})
